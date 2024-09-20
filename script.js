14e70edc16614071ce000d75f8d24b2d998992d3
johnbenedictandaya <benedictandaya86@gmail.com>
2024-09-20 18:13:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -44,3 +44,23 @@ document.getElementById('grade-form').addEventListener('submit', function(event)
         document.getElementById('results').classList.remove('hidden');
     }
 });
+
+let container = document.getElementById('container2');
+let count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container.clientWidth);
+    let topSnow = Math.floor(Math.random() * container.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container.appendChild(div);
+}
